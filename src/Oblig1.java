0a63793d653a7d6a55957ae056d973ca6a7ca8f3
Pratibha Shrestha <pateebha@gmail.com>
2018-09-14 02:09:46 +0200

    small changes

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -11,17 +11,18 @@ public class Oblig1 {
         int[] arr2 = new int[]{3, 3, 4, 5, 5, 6, 7, 7, 7, 8};
         int[] arr3 = new int[]{5, 3, 7, 4, 3, 5, 7, 8, 6, 7};
 
+        // FOR OPPGAVE 1
         System.out.println("Før ");
         printArray(arr);
-
         int maks = maks(arr);
-
         int antallOmbytter = ombyttinger(arrCopy);
         System.out.println("Oppgave 1");
         System.out.println("Maks verdi fra tabellen : " + maks);
         System.out.println("Antall Ombytting : " + antallOmbytter);
         System.out.println("Etter ");
         printArray(arr);
+
+        // FOR OPPGAVE 2
         System.out.println("Oppgave 2");
         printArray(arr2);
         System.out.println("Antall ulike verdier :" + antallUlikeSortert(arr2));
